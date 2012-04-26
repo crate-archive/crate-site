9608332454c911b832468ff9940278c28ad75ff0
Donald Stufft <donald.stufft@gmail.com>
2012-04-26 11:06:56 -0400

    convert the stats view to take advantage of the new power of DownloadDelta

--- crate_project/apps/packages/stats/views.py
+++ crate_project/apps/packages/stats/views.py
@@ -1,8 +1,11 @@
 import collections
+import datetime
 import json
 import time
 
 import isoweek
+
+from dateutil.relativedelta import relativedelta
 
 from django.http import HttpResponse
 from django.views.decorators.cache import cache_page, cache_control
@@ -15,7 +18,7 @@ def fetch_stats(package):
     releases = list(Release.objects.filter(package=package).only("version", "order").order_by("order"))
     specific_releases = set([x.version for x in releases[-8:]])
 
-    deltas = list(DownloadDelta.objects.filter(file__release__in=releases).only("date", "delta", "file__release__version").order_by("date").select_related("file", "file__release"))
+    deltas = list(DownloadDelta.objects.filter(package=package).only("date", "delta", "release_version").order_by("date"))
 
     # @@@ Sanity Checks
     if not deltas:
@@ -24,12 +27,17 @@ def fetch_stats(package):
     data = [{"name": "Other", "data": []}] + [{"name": release.version, "data": []} for release in releases if release.version in specific_releases]
 
     # Get First Week
-    start_week = isoweek.Week.withdate(deltas[0].date)
+    first_date = deltas[0].date
+    max_ago = datetime.date.today() - relativedelta(months=3)
+
+    start_date = first_date if first_date > max_ago else max_ago
+
+    start_week = isoweek.Week.withdate(start_date)
     end_week = isoweek.Week.thisweek()
 
     current = isoweek.Week(start_week.year, start_week.week)
 
-    while current.year <= end_week.year and current.week < end_week.week:
+    while current.year <= end_week.year and (current.week < end_week.week or current.year < end_week.year):
         for x in data:
             x["data"].append({"x": int(time.mktime(current.day(0).timetuple()))})
         current = isoweek.Week(current.year, current.week + 1)
@@ -38,7 +46,8 @@ def fetch_stats(package):
 
     for d in deltas:
         target = int(time.mktime(isoweek.Week.withdate(d.date).day(0).timetuple()))
-        _data[d.file.release.version if d.file.release.version in specific_releases else "Other"][target] = d.delta
+        key = d.release_version if d.release_version in specific_releases else "Other"
+        _data[key][target] = _data[key].get(target, 0) + d.delta
 
     for i in xrange(0, len(data)):
         for j in xrange(0, len(data[i]["data"])):
